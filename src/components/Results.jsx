1e928f2674a5a1d6d87eaecc509799e2b1d02a60
Rafael Seoanes <rafaseoanes9@gmail.com>
2022-12-31 17:39:12 -0500

    styles initialized

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import './Results.css'
 
 
 function Results(props) {
 
   return (
    
-    <div> {props.data.map(obj => (
-        
+    <div className='results'> {props.data.map(obj => (
+
         <div key={obj.show.id}>
-            <img src={obj.show.image.medium} alt={`${obj.show.name}${obj.show.id} show`} />
-            {obj.show.name}
+            <img className='showImage' src={obj.show.image.medium} alt={`${obj.show.name}${obj.show.id} show`} />
+           
+           <div>
+            <h5> {obj.show.name}</h5>
+            </div>
         </div> ))}
       
       </div>
