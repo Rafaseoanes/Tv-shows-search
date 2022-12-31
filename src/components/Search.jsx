a45690dcecf02015afcaa8404d5185410ff23aee
Rafael Seoanes <rafaseoanes9@gmail.com>
2022-12-31 12:33:35 -0500

    Fetching funtionality added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,26 @@
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import React from 'react'
 import Results from './Results'
 
 
 function Search() {
 
-    const searchShows = (value) => {
-        console.log(value)
+    const searchShows = async (value) => {
+   
+        try {
+            let answer = await fetch(`https://api.tvmaze.com/search/shows?q=${value.showName}`)
+            let data = await answer.json()
+            return data
+            
+        } catch (error) {
+            alert('Please try again')
+        }
+    }
+
+    const validations = (value) => {
+        const error = {}
+        if(value.showName.length < 1) error.showName = 'Please enter a Tv show name!'
+        return error;
     }
 
   return (
@@ -16,9 +30,11 @@ function Search() {
             showName: ''
         }}
         onSubmit = { searchShows }
+        validate = { validations }
         >
             <Form>
                 <Field name='showName' type='text'/>
+                <ErrorMessage name='showName' />
                 <button type='submit' >Search</button>
             </Form>
         </Formik>
